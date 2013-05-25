29ea538939f26bd3bdaee3e6d04e3a80b9363a6b
Pete Werner <pewerner@comcast.net>
2013-05-25 10:39:23 -0400

    Post Merge

--- app/controllers/bhcc_controller.rb
+++ app/controllers/bhcc_controller.rb
@@ -26,7 +26,7 @@ end
 
 			rideIds = getMemberRides(memberId, '2013-05-01','2013-05-31' )
 
-			riderData.push(getRideData(rideIds, riderName))
+			riderData.push(getRideData(rideIds, riderName, memberId))
 
 		end
 
@@ -93,25 +93,64 @@ end
 	end
 
 	# Takes an array of ride ids and requests all ride informtion
-	def getRideData(ride_ids, rider_name)
+	def getRideData(ride_ids, rider_name, rider_id)
 
 		moving_time = 0;
 		elevation_gain = 0
 		distance = 0
 
-		ride_ids.each do |ride|
-			
-			url = "http://www.strava.com/api/v2/rides/#{ride}"
-						
-			response = HTTParty.get url,:format  =>"json"
-						
-			ridedata = JSON.parse(response)
-						
-			moving_time = moving_time + Integer(ridedata["ride"]["moving_time"])
-						
-			elevation_gain =  elevation_gain + Integer(ridedata["ride"]["elevation_gain"])
 
-			distance = distance + Integer(ridedata["ride"]["distance"])
+
+
+
+
+		ride_ids.each do |ride|
+
+
+			# check if ride already exists in Database if not request ride info
+
+
+			if !Rides.exists?(:ride_id => ride)
+
+				puts "Adding Ride to model"
+				url = "http://www.strava.com/api/v2/rides/#{ride}"
+						
+				response = HTTParty.get url,:format  =>"json"
+						
+				ridedata = JSON.parse(response)
+						
+				moving_time = moving_time + Integer(ridedata["ride"]["moving_time"])
+						
+				elevation_gain =  elevation_gain + Integer(ridedata["ride"]["elevation_gain"])
+
+				distance = distance + Integer(ridedata["ride"]["distance"])
+			
+			# add rides to the model
+				rides =Rides.new do |r|
+
+					r.ride_id = ride
+					r.rider_id = rider_id
+					r.distance = distance
+					r.rider_name = rider_name
+					r.moving_time = moving_time
+					r.elevation_gain = elevation_gain
+
+				end
+
+				rides.save
+			else
+				
+				  	r = Rides.find(:first, :conditions => [ "ride_id = ?", ride])
+					
+					moving_time = r.moving_time
+					elevation_gain = r.elevation_gain
+					distance = r.distance
+
+
+			end
+
+
+		
 
 
 		end
@@ -119,7 +158,8 @@ end
 	moving_time = (moving_time/3600.0).round(1)
 	elevation_gain = (elevation_gain * 3.28084).round(1)
 	distance = (distance * 0.000621371).round(1)
-		
+
+
 	riderData = { "name" => rider_name, "hours" => moving_time,"Elevation" => elevation_gain, "Distance" => distance }
 
 	return riderData
