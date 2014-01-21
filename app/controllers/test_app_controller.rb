7a2ea1b9cab86ca72799103fcda3473948a9ace3
Pete Werner <pewerner@comcast.net>
2014-01-21 11:14:02 -0500

    added ruby v to gem file'

--- app/controllers/test_app_controller.rb
+++ app/controllers/test_app_controller.rb
@@ -7,7 +7,12 @@ puts "hello from home"
 
 @messagetoView = "Here is My Message"
 
-@myParams = params 
+@myParams = params[:code] 
+
+@response = HTTParty.post("https://www.strava.com/oauth/token",
+    :query => { :client_id => "177", :client_secret=> "ef771e84b333761f8cdf75b70be82aa396862e5e",:code => params[:code] })
+
+
 
 
 end
