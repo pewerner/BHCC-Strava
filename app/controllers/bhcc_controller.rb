4694030bddf5c056266bfdf25966f6bbc6898278
Pete Werner <pewerner@comcast.net>
2014-04-01 08:53:28 -0400

    Changed Dates

--- app/controllers/bhcc_controller.rb
+++ app/controllers/bhcc_controller.rb
@@ -47,7 +47,7 @@ def getRideData(rider_name, rider_id)
 
 	ridedata = RideDatum.where(:athlete_id => Integer(rider_id))
 
-	ridedata1 = ridedata.find(:all, :conditions =>["date(start_date) BETWEEN ? AND ? ", '2014-03-01 00:00:00', '2014-03-31:23:59:00'])
+	ridedata1 = ridedata.find(:all, :conditions =>["date(start_date) BETWEEN ? AND ? ", '2014-04-01 00:00:00', '2014-04-30:23:59:00'])
 
 	ridedata1.each do |r|
 
